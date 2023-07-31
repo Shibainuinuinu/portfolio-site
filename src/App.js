70d8ecb64a78a8784dcdc3d217f3501ce55fb25d
Kelvin <kelvinfang15@gmail.com>
2023-07-31 16:50:10 -0700

    reorganized some files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import logo from './logo.svg';
 import Header from './components/Header';
 import HomePageBoxes from './components/HomePageBoxes';
 import AboutMe from './components/AboutMe';
+import Projects from './components/Projects';
 import { useState } from 'react';
 import './App.css';
 
 
 import kelvinpic1 from './images/kelvinpic1.jpg'
 import kelvinpic2 from './images/kelvinpic2.jpeg'
-import valPic1 from './images/games/valpic1.png'
-import valPic2 from './images/games/valpic2.png'
-import valPic3 from './images/games/valpic3.png'
-import valPic4 from './images/games/valpic4.png'
 
-import ImageCarousel from './components/ImageCarousel';
 
 function App() {
   const [AboutMeCarousel, setAboutMeCarousel] = useState([
@@ -31,49 +26,12 @@ function App() {
     },
   ])
 
-  const [valorantCarousel, setValorantCarousel] = useState([
-    {
-      index: 0,
-      src: valPic1,
-      alt: "valPic1",
-      'data-active': true,
-    },
-    {
-      index: 1,
-      src: valPic2,
-      alt: "valPic2",
-      'data-active': false,
-    },
-    {
-      index: 2,
-      src: valPic3,
-      alt: "valPic3",
-      'data-active': false,
-    },
-    {
-      index: 3,
-      src: valPic4,
-      alt: "valPic4",
-      'data-active': false,
-    },
-  ])
-  const valDescrip = `
-  This is my top 10 winning website from my internship at 
-  Code Tenderloin. This project utilizes HTML, CSS, and Javascript, 
-  and was the first time I ever made a fully functional website.
-  It is based on the official Valorant website.`
   return (
     <div className="App">
         <Header />
         <AboutMe carousel={AboutMeCarousel}/>
         <div className='main'>
-          <h1 className="projectHeading">Projects</h1>
-          <div className='projects'>
-            <div className='val'>
-              <HomePageBoxes title="Valorant Website" description={valDescrip} pos='left'/>
-              <ImageCarousel images={valorantCarousel} style={{width: '40vw', marginLeft: '1vw'}}/>
-            </div>
-          </div>
+          <Projects/>
           <HomePageBoxes title="Connect" description="These are my projects" pos='middle' style={{marginBottom: '5vh'}}/>
         </div>
     </div>
---
+++ src/components/Projects.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import { useState } from 'react'
+
+import ImageCarousel from './ImageCarousel';
+import HomePageBoxes from './HomePageBoxes'
+
+
+import valPic1 from '../images/games/valpic1.png'
+import valPic2 from '../images/games/valpic2.png'
+import valPic3 from '../images/games/valpic3.png'
+import valPic4 from '../images/games/valpic4.png'
+import wAndC1 from '../images/wAndC/wAndC1.png'
+import wAndC2 from '../images/wAndC/wAndC2.png'
+
+
+
+const Projects = () => {
+    const [valorantCarousel, setValorantCarousel] = useState([
+        {
+          index: 0,
+          src: valPic1,
+          alt: "valPic1",
+          'data-active': true,
+        },
+        {
+          index: 1,
+          src: valPic2,
+          alt: "valPic2",
+          'data-active': false,
+        },
+        {
+          index: 2,
+          src: valPic3,
+          alt: "valPic3",
+          'data-active': false,
+        },
+        {
+          index: 3,
+          src: valPic4,
+          alt: "valPic4",
+          'data-active': false,
+        },
+      ])
+      const [wAndCCarousel, setwAndCCarousel] = useState([
+        {
+            index: 0,
+            src: wAndC1,
+            alt: "wAndC1",
+            'data-active': true,
+          },    
+          {
+            index: 1,
+            src: wAndC2,
+            alt: "wAndC2",
+            'data-active': false,
+          },    
+      ])
+
+      const valDescrip = `
+      This is my top 10 winning website from my internship at 
+      Code Tenderloin. This project utilizes HTML, CSS, and Javascript, 
+      and was the first time I ever made a fully functional website.
+      It is based on the official Valorant website.`
+      const wAndC = `
+      This is my top 3 projects from my spring Python class at Mission Bit. It won 
+      the category for most technical project. This project uitlizes python and APIs 
+      including the OpenWeatherApp API and Yelp Fusion API. It suggests clothes based
+      on the the current weather in SF and suggests local businesses to purchase these 
+      clothes from.`
+  return (
+    <>
+        <h1 className="projectHeading">Projects</h1>
+        <div className='projects'>
+            <div className='singleproject'>
+                    <HomePageBoxes title="Valorant Website" description={valDescrip} pos='left'/>
+                    <ImageCarousel images={valorantCarousel} style={{width: '40vw', marginLeft: '1vw'}}/>
+            </div>
+            <div className='singleproject'>
+                <ImageCarousel images={wAndCCarousel} style={{width: '40vw', marginRight: '1vw', objectPosition: ''}}/>   
+                <HomePageBoxes title="Weather And Clothes App" description={wAndC} pos='right'/>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Projects
