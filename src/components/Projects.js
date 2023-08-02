3124b555b4e61d9de7b7e74d4ca78b61e76be2ba
Kelvin <kelvinfang15@gmail.com>
2023-08-01 20:14:13 -0700

    added chatbot project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,57 +1,11 @@
 import React from 'react'
-import { useState } from 'react'
 
-import ImageCarousel from './ImageCarousel';
 import HomePageBoxes from './HomePageBoxes'
-
-import valPic1 from '../images/games/valpic1.png'
 import valPic2 from '../images/games/valpic2.png'
-import valPic3 from '../images/games/valpic3.png'
-import valPic4 from '../images/games/valpic4.png'
 import wAndC1 from '../images/wAndC/wAndC1.png'
-import wAndC2 from '../images/wAndC/wAndC2.png'
+import googleChatBotPic from '../images/googleChatBot.png'
 
 const Projects = () => {
-    const [valorantCarousel, setValorantCarousel] = useState([
-        {
-          index: 0,
-          src: valPic1,
-          alt: "valPic1",
-          'data-active': true,
-        },
-        {
-          index: 1,
-          src: valPic2,
-          alt: "valPic2",
-          'data-active': false,
-        },
-        {
-          index: 2,
-          src: valPic3,
-          alt: "valPic3",
-          'data-active': false,
-        },
-        {
-          index: 3,
-          src: valPic4,
-          alt: "valPic4",
-          'data-active': false,
-        },
-      ])
-      const [wAndCCarousel, setwAndCCarousel] = useState([
-        {
-            index: 0,
-            src: wAndC1,
-            alt: "wAndC1",
-            'data-active': true,
-          },    
-          {
-            index: 1,
-            src: wAndC2,
-            alt: "wAndC2",
-            'data-active': false,
-          },    
-      ])
 
       const valDescrip = `
       This is my top 10 winning website from my internship at 
@@ -64,6 +18,10 @@ const Projects = () => {
       including the OpenWeatherApp API and Yelp Fusion API. It suggests clothes based
       on the the current weather in SF and suggests local businesses to purchase these 
       clothes from.`
+      const gcbot = `This is my Google Chatbot project our CSI Google Tech Cohort worked together 
+      to create as the culminating project for our internship. We utilized Google's Dialogflow API,
+      and various programming laanguages to design the UI and the Google Help site data. This chatbot
+      makes navigating and answering users' questions faster on the Google Help page.`
       const  valStyles = {
         backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${valPic2})`,
         objectFit: 'cover',
@@ -71,6 +29,11 @@ const Projects = () => {
       }
       const wAndCStyles = {
         backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${wAndC1})`,
+        objectFit: 'cover',
+        objectPosition: 'center',     
+      }
+      const gcbotStyles = {
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${googleChatBotPic})`,
         objectFit: 'cover',
         objectPosition: 'center',     
       }
@@ -83,6 +46,9 @@ const Projects = () => {
             </div>
             <div className='singleproject'>
                 <HomePageBoxes title="Weather And Clothes App" description={wAndC} pos='middle' canGrow={true} style={wAndCStyles}/>
+            </div>
+            <div className='singleproject'>
+                <HomePageBoxes title="Google Help Chatbot" description={gcbot} pos='middle' canGrow={true} style={gcbotStyles}/>
             </div>
         </div>
     </>
